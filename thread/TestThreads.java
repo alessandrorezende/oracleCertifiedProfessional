1fd681aaa8e11446625ac89e25a579335cffb63f
Alessandro Rezende <alessandropereirarezende@gmail.com>
2018-12-01 18:48:38 -0200

    Test Threads.

--- thread/TestThreads.java
+++ thread/TestThreads.java
@@ -2,13 +2,17 @@
 public class TestThreads{
 
     public static void main(String ... args){
-        //--------------------------------------------------------------
-      //Não é a melhor forma de executar uma thread
-      ThreadFirst thread = new ThreadFirst();
-      thread.run();
       //--------------------------------------------------------------
-      //Forma correta de passar um trabalho para uma thread executar
+      //1ex: Não é a melhor forma de executar uma thread
+      //a execução é passada para pilha de chamadas atual NÃO
+      //uma nova pilha como no segundo exemplo.
+      ThreadFirst thread = new ThreadFirst();
+      thread.run(); //Válido, mas não começa uma nova thread
+      
+      //--------------------------------------------------------------
+      //2ex: Forma correta de passar um trabalho para uma thread executar
       //em uma nova pilha de chamadas.
+
       //trabalho a ser feito
       MyRunnable job = new MyRunnable();
       //thread executa o trabalho
