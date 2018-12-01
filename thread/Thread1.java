340d63911cc6cedc8b49438a5764f5de29950dc6
Alessandro Rezende <alessandropereirarezende@gmail.com>
2018-12-01 17:19:26 -0200

    Exercise 1 Thread.

--- thread/Thread1.java
+++ thread/Thread1.java
@@ -1,8 +1,20 @@
 import java.lang.Thread;
 
+/*
+Não é uma boa pratica de OO extender Thread quando se quer apenas
+que uma Thread execute algum trabalho o correto é implementar a interface Runnable.
+Deve ser extendido Thread apenas quando quer ter uma versão mais especializada
+de thread.
+*/
 class ThreadFirst extends Thread {
   public void run(){
-      System.out. println("Important job running in my tread");
+      System.out. println("Important job running in my first tread!");
+  }
+}
+
+class ThreadSecond implements Runnable {
+  public void run(){
+      System.out. println("Important job running in my second tread!");
   }
 }
 
@@ -10,5 +22,8 @@ class Thread1 extends Thread {
   public static void main(String ... args){
     ThreadFirst thread = new ThreadFirst();
     thread.run();
+
+    ThreadSecond thread2 = new ThreadSecond();
+    thread2.run();
   }
 }
